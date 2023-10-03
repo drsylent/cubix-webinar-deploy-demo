2a9750f8e8bcef7d38512978767d2bb42d55dbe2
Dávid Csendes <sylence96@gmail.com>
2023-10-03 09:33:27 +0200

    Update version number for local running

--- local/db.ps1
+++ local/db.ps1
@@ -1,3 +1,3 @@
-java -jar $ENV:DB_WORKSPACE\target\cubix-webinar-db-demo-0.0.1-SNAPSHOT.jar `
+java -jar $ENV:DB_WORKSPACE\target\cubix-webinar-db-demo-23.10.1-SNAPSHOT.jar `
     --server.port=8081 --management.server.port=9081 `
     --spring.datasource.url=jdbc:postgresql://localhost:5432/postgres --spring.datasource.username=user --spring.datasource.password=password
